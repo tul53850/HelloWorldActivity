6f8fe57afcc4177fb98000b8312806fefce8afa2
Jason Hankins <tul53850@temple.edu>
2024-01-30 12:11:39 -0500

    OnClick listener to update displayText with nameEdit

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -24,7 +24,10 @@ class MainActivity : AppCompatActivity() {
 
         
         // Respond to button click event per specifications
-
+        clickMeButton.setOnClickListener{
+            //Log.d("MainAct", "Button Click")
+            displayTextView.text = "Hello, ${nameEditText.text}"
+        }
 
     }
 }
